787e23fe86dbcb5b9ae4034ff6a7b23f6c630ed1
Ankan Basu <ankanbasu10@gmail.com>
2026-06-03 04:06:53 +0530

    Webhook Test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,6 +6,6 @@ package com.example;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Webhook Test");
+      System.out.println("Webhook Test 2");
     }
 }
